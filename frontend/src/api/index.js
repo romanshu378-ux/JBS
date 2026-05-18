9d49fee8cb0bb2fabcc3e35b5435067391740acb
Your Name <youremail@gmail.com>
2026-05-19 00:21:29 +0530

    Fix frontend API URL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend URL
+  baseURL: 'https://jbs-pazg.onrender.com/api',
 });
 
 export default API;
