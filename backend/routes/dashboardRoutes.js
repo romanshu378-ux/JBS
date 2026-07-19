593bba2d8d09b40448626f34a9699a9ac5d7deb3
Your Name <youremail@gmail.com>
2026-07-19 11:17:26 +0530

    Reindex repository

--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDashboardData } = require('../controllers/dashboardController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/')
-  .get(protect, getDashboardData);
-
-module.exports = router;
